1a7653e21a162413e5d7a3648f958520a92b120d
BernáthMilán(SZF_N_2 <BernthMiln(SZF_N_2@192.168.228.5>
2026-09-16 16:31:25 +0200

    alap állapot metódusokkal és tömbökkel

---
+++ src/app/Program.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app;
+
+/**
+ *
+ * @author BernáthMilán(SZF_N_2
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    static String[] pakli = new String[22];
+    static String[] szinek = {"P", "T", "Z", "M"};
+    static String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 3; i++) {
+            kirak();
+            melyik();
+            kever();
+        }
+        ezVolt();
+    }
+    
+    public static void kirak(){
+        
+    }
+    
+    public static void melyik(){
+        
+    }
+    
+    public static void kever(){
+     
+    }
+    
+    public static void ezVolt(){
+        
+    }
+    
+}
