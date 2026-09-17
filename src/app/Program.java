f4f99906607ef7ee6b87c2f7e5baae2e28f73d01
BernáthMilán(SZF_N_2 <BernthMiln(SZF_N_2@192.168.228.5>
2026-09-17 15:09:59 +0200

    még nem kész

--- src/app/Program.java
+++ src/app/Program.java
@@ -4,6 +4,8 @@
  */
 package app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author BernáthMilán(SZF_N_2
@@ -14,33 +16,75 @@ public class Program {
      * @param args the command line arguments
      */
     static String[] pakli = new String[22];
+    static Scanner scr = new Scanner(System.in);
+    static String[] a = new String[22];
     
     public static void main(String[] args) {
         pakliFeltolt();
         for (int i = 0; i < 3; i++) {
-            kirak();
-            melyik();
-            kever();
+            if(i == 0){
+                kirak(pakli);
+            } else {
+                kirak(a);
+            }
+            int tipp = melyik();
+            kever(tipp);
         }
         ezVolt();
     }
     
-    public static void kirak(){
-        for (int i = 0; i < pakli.length; i++) {
-            System.out.println(pakli[i]);
+    public static void kirak(String[] lista){
+        for (int i = 1; i < lista.length; i++) {
+            System.out.print(lista[i] + "\t");
+            if (i % 3 == 0) {
+                System.out.println();
+            }
         }
+        System.out.println();
     }
     
-    public static void melyik(){
-        
+    public static int melyik(){
+        System.out.print("Melyik oszlopban van a kiválasztott lap?: ");
+        int tipp = scr.nextInt();
+        while (tipp < 1 || tipp > 3) {            
+            System.out.print("1-3 közötti értéket adj meg: ");
+            tipp = scr.nextInt();
+        }
+        System.out.println();
+        return tipp;
     }
     
-    public static void kever(){
-     
+    public static String[] kever(int tipp){
+        switch (tipp) {
+            case 1:
+                for (int i = 1; i < 7; i++) {
+                   a[i]= pakli[20-((i-1)*3)];
+                   a[i]= pakli[19-((i-1)*3)];
+                   a[i]= pakli[21-((i-1)*3)];
+                }
+                break;
+            case 2: 
+                for (int i = 1; i < 7; i++) {
+                   a[i]= pakli[19-((i-1)*3)];
+                   a[i]= pakli[20-((i-1)*3)];
+                   a[i]= pakli[21-((i-1)*3)]; 
+                }
+                break;
+            case 3: 
+                for (int i = 1; i < 7; i++) {
+                   a[i]= pakli[19-((i-1)*3)];
+                   a[i]= pakli[21-((i-1)*3)];
+                   a[i]= pakli[20-((i-1)*3)];
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
+        return a;       
     }
     
     public static void ezVolt(){
-        
+        System.out.println("A " + pakli[11] + " lapott válsztottad");
     }
     
     public static String[] pakliFeltolt(){
